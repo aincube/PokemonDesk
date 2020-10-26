2319206a4fab97e8d9007fbfc71802add92a22d2
aincube <aincube@gmail.com>
2020-10-27 00:44:36 +0100

    Homework 1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import s from './App.modules.scss';
+
+const App = () => {
+    return (
+        <div className={s.header}>
+            Yes, we did it! This is App component!
+        </div>
+    )
+}
+
+export default App;
