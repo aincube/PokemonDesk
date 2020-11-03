3c7b56765153325a75429a0a709a4774033ef192
aincube <aincube@gmail.com>
2020-11-03 01:50:14 +0100

    Added ESLint and Prettier

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import cn from 'classnames';
+
+import s from './App.module.scss';
+
+const App = () => {
+  return <div className={cn(s.header)}>Yes, we did it! This is App component!</div>;
+};
+
+export default App;
