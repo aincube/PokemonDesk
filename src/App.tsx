efbd626bcedb89e44085bca9440214473e010002
aincube <aincube@gmail.com>
2020-11-10 20:52:08 +0100

    Работают роуты через hookrouter
    WIP надо убрать из проекта старый роутер, сверстать нотфаунд и подчистить коменты

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
+// import React from 'react';
+
+// import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { useRoutes } from 'hookrouter';
 import React from 'react';
+import NotFoundPage from './pages/NotFound';
 
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+// import Header from './components/Header';
+// import Footer from './components/Footer';
 
-import Header from './components/Header';
-import Footer from './components/Footer';
-
-import HomePage from './pages/Home';
-import Pokedex from './pages/Pokedex';
+// import HomePage from './pages/Home';
+// import Pokedex from './pages/Pokedex';
+import routes from './routes';
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/Pokedex" component={Pokedex} />
-      </Switch>
-      <Footer />
-    </BrowserRouter>
-  );
+  // return (
+  //   <BrowserRouter>
+  //     <Header />
+  //     <Switch>
+  //       <Route exact path="/" component={HomePage} />
+  //       <Route path="/Pokedex" component={Pokedex} />
+  //     </Switch>
+  //     <Footer />
+  //   </BrowserRouter>
+  // );
+  const match = useRoutes(routes);
+  return match || <NotFoundPage />;
 };
 
 export default App;
